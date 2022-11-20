0d9dfe7b366fb7c11df1b055a18e825145b38cf8
IdeaLim <wiuand@naver.com>
2022-11-20 13:50:03 +0900

    Edit: requirements, and SimilarImageFinder using area, face verification

--- Cross-cutting-with-face/SimilarImageFinder.py
+++ Cross-cutting-with-face/SimilarImageFinder.py
@@ -1,13 +1,15 @@
-from yoloface.face_detector import YoloDetector
+from yoloface import face_detector
 from util.distance import findEuclideanDistance
 from itertools import combinations
 from util.VideoCapture import VideoCapture
 from util.plot import plot_by_cv2
 import torch
 from similarImageFinder_functions import *
+from verifier import FaceVerifier, VGGFace
 
 class SimilarImageFinder:
     def __init__(self, detector, video_name, video_path, period=30, video_capture=True):
+        self.video_name = video_name
         if video_capture:
             # 영상 -> 이미지
             VideoCapture(period, video_name, video_path)
@@ -18,7 +20,7 @@ class SimilarImageFinder:
         # 유사한 이미지 탐색
         similarity_list = self.find_similarity_images(df)
         # 이미지 출력
-        plot_by_cv2(similarity_list, video_name)
+        plot_by_cv2(similarity_list, video_name, 30)
 
     def _make_df_detection_by_detector(self, detector, video_name):
         """
@@ -29,11 +31,11 @@ class SimilarImageFinder:
         if detector == 'yoloface':
             try:
                 torch.tensor(1).cuda()
-                yolo_detector = YoloDetector(target_size=720, gpu=1, min_face=90)
+                yolo_detector = face_detector.YoloDetector(target_size=720, gpu=1, min_face=90)
                 print('gpu 사용')
-                df_detection = detect_images_by_gpu(yolo_detector, video_name, batch_size=64)
+                df_detection = detect_images_by_gpu(yolo_detector, video_name, batch_size=32)
             except:
-                yolo_detector = YoloDetector(target_size=720, gpu=-1, min_face=90)
+                yolo_detector = face_detector.YoloDetector(target_size=720, gpu=-1, min_face=90)
                 print('cpu 사용')
                 df_detection = detect_images_by_cpu(yolo_detector, video_name)
         else:
@@ -43,12 +45,13 @@ class SimilarImageFinder:
         # filter 된 dataframe
         df_filtered = filter_df(df_detection)
         # 데이터프레임 저장
-        df_filtered.to_parquet(f'./dataset/{video_name}/detection_data/yoloface_data.parquet')
+        df_filtered.to_parquet(f'./dataset/{video_name}/detection_data/yoloface_data.parquet', engine='pyarrow')
 
         return df_filtered
 
     def find_similarity_images(self, df_filtered):
         similarity_list = []
+        model = VGGFace.loadModel() # Face Verification 을 위한 VGGFace Model 불러오기
         for frame in df_filtered.index.unique():
             df_temp = df_filtered.loc[frame]
             # 감지된 사람 수
@@ -62,14 +65,24 @@ class SimilarImageFinder:
 
             for selected_video_nums in list(combinations(video_num_list, 2)):
                 videonum1, videonum2 = selected_video_nums[0], selected_video_nums[1]
-
-                landmarks = list(
-                    df_temp.loc[(df_temp['video_num'] == videonum1) | (df_temp['video_num'] == videonum2)]['landmarks'])
-                dis = findEuclideanDistance(landmarks[0], landmarks[1], detect_person_num)
-                if dis < dis_min:
-                    dis_min = dis
-                    selected_video = selected_video_nums
-            similarity_list.append((frame, selected_video, dis_min))
+                boxes = list(df_temp.loc[(df_temp['video_num'] == videonum1) | (df_temp['video_num'] == videonum2)]['boxes'])
+                imgs_path = [f'./dataset/{self.video_name}/frame/{frame}/{videonum1}.jpg',
+                             f'./dataset/{self.video_name}/frame/{frame}/{videonum2}.jpg']
+                # 2개의 이미지에서 가장 큰 얼굴 간의 비율, face verification 을 위한 crop face image
+                area_fraction, crop_face1, crop_face2 = get_max_area_fraction_and_crop_faces(boxes, imgs_path)
+                verified = FaceVerifier.verify(crop_face1, crop_face2, model)['verified']
+                print(verified)
+                # compare area_fraction and face recognition
+                if 0.8 < area_fraction < 1.2 and verified:
+                    landmarks = list(
+                        df_temp.loc[(df_temp['video_num'] == videonum1) | (df_temp['video_num'] == videonum2)]['landmarks'])
+                    dis = findEuclideanDistance(landmarks[0], landmarks[1], detect_person_num)
+                    print(dis)
+                    if dis < dis_min:
+                        dis_min = dis
+                        selected_video = selected_video_nums
+            if selected_video is not None:
+                similarity_list.append((frame, selected_video, dis_min))
         similarity_list = sorted(similarity_list, key=lambda x: x[2])
         return similarity_list
 
@@ -77,5 +90,5 @@ class SimilarImageFinder:
 if __name__ == '__main__':
     video_path = 'C:/Users/JungSupLim/Desktop/video'
     # video capture 생략하고 싶으면 False
-    SimilarImageFinder('yoloface', 'idle_tomboy', video_path, period=10, video_capture=True)
+    SimilarImageFinder('yoloface', 'idle_tomboy', video_path, period=30, video_capture=False)
 
--- Cross-cutting-with-face/similarImageFinder_functions.py
+++ Cross-cutting-with-face/similarImageFinder_functions.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
+
 def get_all_images(video_name):
     """
     dataset 폴더 안에 있는 모든 이미지 경로를 반환하는 함수입니다.
@@ -18,7 +19,7 @@ def _zip_person(boxes, points, gpu=True):
     """
     box 와 landmark 의 리스트를 각 사람의 box 와 landmark 로 합치는 함수입니다.
     """
-    if gpu != True:
+    if not gpu:
         person = zip(boxes[0], points[0])
         return list(person)
     else:
@@ -32,16 +33,33 @@ def _sort_by_x1(boxes):
     boxes = sorted(boxes, key=lambda x: x[0][0])
     return boxes
 
-def get_max_area(boxes):
+def get_max_area_fraction_and_crop_faces(boxes, imgs_path):
+    """
+    2개의 이미지에서 가장 영역이 큰 사람을 찾고, area fraction 을 반환하는 함수 입니다.
+    """
     area_list = []  # 2개의 영상에서 각각 제일 큰 크기의 face area 를 담을 리스트
-    for people in boxes:
+    crop_face_list = [] # 2개의 Crop Face Image 를 담을 리스트
+    for faces in boxes: # people : Image 안에 있는 Face 좌표 리스트
         max_area = 0
-        for person in people:
-            x1, y1, x2, y2 = person[0], person[1], person[2], person[3]
+        max_face = None
+        for face in faces:
+            x1, y1, x2, y2 = face[0], face[1], face[2], face[3]
             area = (x2-x1)*(y2-y1)
-            if max_area < area: max_area = area
+            if max_area < area:
+                max_area = area
+                max_face = face
         area_list.append(max_area)
-    return area_list
+        crop_face_list.append(max_face)
+    # Face crop 이미지로 저장
+    crop_face_list = [np.array(Image.open(imgs_path[i]).crop((face[0], face[1], face[2], face[3])))
+                      for i, face in enumerate(crop_face_list)]
+
+    # img1 = cv2.cvtColor(crop_face_list[0], cv2.COLOR_BGR2RGB)
+    # cv2.imshow('img1', img1)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+
+    return area_list[0]/area_list[1], crop_face_list[0], crop_face_list[1]
 
 def generate_batch(lst, batch_size):
     """  Yields batch of specified size """
@@ -137,3 +155,4 @@ def filter_df(df):
         mask = df1.loc[frame_num].duplicated(['detect_person_num'], keep=False)
         df_filtered = pd.concat([df_filtered, df1.loc[mask]])
     return df_filtered
+
